6fabd6cd164a8944a89b9bd2979fee2736515214
Lucas Theisen <lucastheisen@pastdev.com>
2016-01-18 17:13:01 -0500

    fixed bug when no releaseCallback was provided

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 'use strict';
 
 var gulp = require('gulp');
+var Q = require('q');
 var release = require('.').release
+var spawn = require('child_process').spawn;
 
 gulp.task('release', function() {
-    return release();
+    return release(
+        {
+            releaseCallback: function() {
+                var deferred = Q.defer();
+                spawn('npm', ['publish'], {stdio: 'inherit'})
+                    .on('close', function(err) {
+                        if (err) {
+                            deferred.reject(err);
+                        }
+                        deferred.resolve();
+                    });
+                return deferred.promise;
+            }
+        });
 });
